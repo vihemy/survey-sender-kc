06dfc21802659632674fe3a80cd7b74e252d0a12
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-07-13 09:02:06 +0200

    added regex-check to import_numbers_from_excel_as_list and deleted commented out code

--- phonenumber_extractor.py
+++ phonenumber_extractor.py
@@ -1,39 +1,39 @@
 #External libraries
 import openpyxl
+import re
 
 # Internal libraries
 from phonenumber_class import PhoneNumber
 import printer
 
+
+def check_if_cell_content_is_valid(cell):
+    if check_if_cell_is_empty(cell):
+        return False
+    elif check_if_cell_contains_inappropriate_characters(cell):
+        return False
+    else:
+        return True
+
+def check_if_cell_is_empty(cell):
+    return bool (cell.value == None)
+
+def check_if_cell_contains_inappropriate_characters(cell):
+    return bool(re.search(r'[^0-9+]', cell.value)) # checks if cell value contains anything but numbers and '+'
+
+
 def import_numbers_from_excel_as_list(excel_file, first_row, first_col):
-    # Sets workbook and sheet for openpyxl
     wb = openpyxl.load_workbook(excel_file)
     sheet = wb.active # gets active/first sheet-name of workbook
     phone_numbers = [] # resets list
+
     # loops through specified columns and rows.
     for col in range(first_col, sheet.max_column+1):
         for row in range(first_row, sheet.max_row+1):
             cell = sheet.cell(row, col)
-            if type(cell.value) == int: # if cell.value is not empty and is an integer
+            if check_if_cell_content_is_valid(cell): # if cell.value is not empty and does not contains letters, add as phoenumber-object
                 phone_number = PhoneNumber(cell.value) # creates PhoneNumber-object
                 phone_numbers.append(phone_number) # appends phone_number to list
     wb.close() # closes workbook
-
     printer.print_imported_numbers(phone_numbers)
     return phone_numbers
-
-
-#----------------------------------
-
-# REMOVED BECAUSE OF SYNC-ISSUES WHEN EDITING FILE ON ONEDRIVE THROUGH SCRIPT
-
-# def clear_sheet(excel_file, first_row, first_col):
-#     wb = openpyxl.load_workbook(excel_file)
-#     sheet = wb.active
-#     for col in range(first_col, sheet.max_column+1):
-#         for row in range(first_row, sheet.max_row+1):
-#             cell = sheet.cell(row, col)
-#             cell.value = None
-#     wb.save(excel_file)
-#     wb.close()
-#     print("Excel-ark er ryddet")
