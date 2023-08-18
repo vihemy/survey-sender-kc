d41e2417ca7cd48b81ad7056a2e523b3de804cc9
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-08-18 12:43:12 +0200

    Added docstrings

--- scripts/phonenumber_extractor.py
+++ scripts/phonenumber_extractor.py
@@ -8,6 +8,7 @@ import printer
 
 
 def check_if_cell_content_is_valid(cell):
+    """Check if cell content is valid. Returns True if valid, False if not."""
     if check_if_cell_is_empty(cell):
         return False
     elif check_if_cell_contains_less_than_8_digits(cell):
@@ -19,22 +20,26 @@ def check_if_cell_content_is_valid(cell):
 
 
 def check_if_cell_is_empty(cell):
+    """Check if cell is empty. Returns True if empty, False if not."""
     return bool(cell.value == None)
 
 
 def check_if_cell_contains_less_than_8_digits(cell):
+    """Check if cell contains less than 8 digits. Returns True if less than 8 digits, False if not."""
     return bool(len(cell.value) < 8)
 
 
 def check_if_cell_contains_inappropriate_characters(cell):
+    """Check if cell contains characters other than numbers and "+". Returns True if inappropriate characters, False if not."""
     # checks if cell value contains anything but numbers and '+'
     return bool(re.search(r'[^0-9+]', cell.value))
 
 
 def import_numbers_from_excel_as_list(excel_file, first_row, first_col):
+    """Import phone numbers from excel file and return as list of PhoneNumber-objects"""
+    phone_numbers = []  # resets list
     wb = openpyxl.load_workbook(excel_file)
     sheet = wb.active  # gets active/first sheet-name of workbook
-    phone_numbers = []  # resets list
 
     # loops through specified columns and rows.
     for col in range(first_col, sheet.max_column+1):
