06e1007146a441e512d9afb49717f3fcee153fe8
Victor Hempel-Mydtskov <victor@mydtskov.com>
2023-08-18 22:21:17 +0200

    updated print-statements

--- scripts/printer.py
+++ scripts/printer.py
@@ -1,12 +1,12 @@
 
 def print_greeting():
-    greeting_string = "Velkommen til Survey Sender v.2.0.\n\nFor at sende spørgeskemaer, gør følgende:\n1. Vælg excel-filen med telefonnumre fra OneDrive-mappen\n2. Vælg link-type (vælg live link, med mindre du tester sending til dit eget nummer\n3. Tryk på Send-knappen\n4. Vent på, at programmet har kørt færdig\n5. Åbn excel-filen og slet telefonnumrene der er blevet sendt til. \n\nHvis du har spørgsmål, så kontakt Victor på 52 13 72 34. Hvis du oplever fejl, så send et billede af fejlmeddelelsen og så meget af den øvrige skærm som muligt til Victor på teams eller mail vhm@kattegatcentret.dk"
+    greeting_string = "Velkommen til Survey Sender v.2.0.\n\nFor at sende spørgeskemaer:\n1. Vælg excel-filen med telefonnumre fra OneDrive-mappen\n2. Vælg link-type (vælg live link, med mindre du tester sending til dit eget nummer)\n3. Tryk på Send-knappen\n4. Vent på, at programmet har kørt færdig (Hold firefox-vinduet åbent mens det kører)\n5. Åbn excel-filen og slet telefonnumrene der er blevet sendt til. \n\nHvis du har spørgsmål, så kontakt Victor på 52 13 72 34. Hvis du oplever fejl, så send et billede af fejlmeddelelsen og så meget af den øvrige skærm som muligt til Victor på teams eller mail vhm@kattegatcentret.dk"
     print(greeting_string)
 
 
 def print_imported_numbers(phone_numbers):
     print("----------------------------------")
-    print("ANTAL IMPORTEDE TELEFONNUMRE: " + str(len(phone_numbers)) + "\n")
+    print(f"FØLGENDE {len(phone_numbers)} TELEFONNUMRE ER IMPORTERET \n")
     print_list(phone_numbers)
 
 
@@ -32,13 +32,14 @@ def print_all_sent(sent_to_count):
 
 def print_not_all_sent(phone_numbers, sent_to):
     print("----------------------------------")
-    print(f"{len(phone_numbers) - len(sent_to)} NUMRE BLEV IKKE SENDT. FØLGENDE NUMRE ER DOG SENDT:")
+    print(
+        f"FØLGENDE {len(sent_to)} UD AF {len(phone_numbers)} NUMRE ER SENDT TIL:")
     print_list(sent_to)
-    print("Slet overstående numre fra excel-arket og prøv igen.")
+    print("Slet disse numre fra excel-arket og prøv igen med resten.")
 
 
 def print_permission_error_message():
-    permission_error_string = "Der er ikke adgang til excel-filen. Luk excel-filen, hvis du har den åbent et andet sted på computeren og prøv igen."
+    permission_error_string = "Der er ikke adgang til excel-filen. Sikr dig at filen ikke er åbent i et andet vindue og prøv igen."
     print(permission_error_string)
 
 
