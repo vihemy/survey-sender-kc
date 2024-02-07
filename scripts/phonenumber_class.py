b14dca884fd276fc7c6f0fe4a5c38efe1beef482
Victor Hempel-Mydtskov <victor@mydtskov.com>
2024-02-07 12:47:48 +0100

    Rewritten to avoid DRY

--- scripts/phonenumber_class.py
+++ scripts/phonenumber_class.py
@@ -3,15 +3,16 @@ import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import COUNTRY_CODE_TO_REGION_CODE
 
-
 class PhoneNumber:
     '''
-    Class to handle phone numbers and their characeristics
+    Class to handle phone numbers and their characteristics.
 
     Attributes
     ----------
     number : str
         Phone number (e.g. +4512345678)
+    parsed_number : PhoneNumber object
+        Parsed phone number object from the phonenumbers library
 
     Methods
     -------
@@ -29,73 +30,38 @@ class PhoneNumber:
         Returns country name as str (e.g. 'Denmark')
     '''
 
-    def __init__(self, number):
-        self.number = self._replace_00_with_plus(number)
+    COUNTRY_LANGUAGE_MAP = {
+        45: 'DAN', # Denmark
+        49: 'DEU', # Germany
+        43: 'DEU', # Austria
+        41: 'DEU', # Switzerland
+        # Add other mappings as needed
+    }
 
-    def _replace_00_with_plus(self, number):
-        """Replace any leading '00' in with '+' and return number."""
-        if number[0:2] == "00":
-            number = "+" + number[2:]
-        return number
+    def __init__(self, number):
+        self.number = number.replace("00", "+", 1)
+        self.parsed_number = phonenumbers.parse(self.number, None) if self.number.startswith('+') else None
 
     def number_as_int(self):
         """Return phone number as int (e.g. 4512345678)"""
-        number_as_int = int(self.number)
-        return number_as_int
+        return int(self.number)
 
     def national_number(self):
         """Return national number as int (e.g. 12345678)"""
-        # If country code is noted (signified by use of +), country code is stripped, leaving only national number - OBS! THIS IS STRING, NOT INT! PLUS-SIGN CAN NOT BE DETECTED IN INT!
-        if self._starts_with_plus(self.number):
-            # No need for second argument, as country code is present in i.)
-            parsed_number = phonenumbers.parse(self.number, None)
-            national_number = parsed_number.national_number
-
-        # If no country code is present (signified by lack of "+") all of self.number is national number
-        else:
-            national_number = int(self.number)
-        return national_number
+        return self.parsed_number.national_number if self.parsed_number else int(self.number)
 
     def country_code(self):
         """Return country code as int (e.g. 45). If no country code is present, country code is set to 45 (Denmark)"""
-        # if '+' in self.number:
-        if self._starts_with_plus(self.number):
-            parsed_number = phonenumbers.parse(self.number, None)
-            country_code = parsed_number.country_code
-        else:
-            country_code = 45
-        return country_code
-
-    def _starts_with_plus(self, number):
-        """Return True if number starts with '+', False if not"""
-        if number[0] == '+':
-            return True
-        else:
-            return False
+        return self.parsed_number.country_code if self.parsed_number else 45
 
     def survey_language(self):
         """Return survey language as str (e.g. 'DAN')"""
-        if self.country_code() == 45:
-            survey_language = 'DAN'
-        elif self.country_code() == 49 or self.country_code() == 43 or self.country_code() == 41:
-            survey_language = 'DEU'
-        else:
-            survey_language = 'ENG'
-        return survey_language
+        return self.COUNTRY_LANGUAGE_MAP.get(self.country_code(), 'ENG')
 
     def region_code(self):
         """Return region code(s) as tuple of str (e.g. 'DK')"""
-        region_code = COUNTRY_CODE_TO_REGION_CODE[self.country_code()]
-        return region_code
+        return COUNTRY_CODE_TO_REGION_CODE.get(self.country_code(), ('Unknown',))
 
     def country_name(self):
         """Return country name as str (e.g. 'Denmark')"""
-        if '+' in self.number:
-            # Gets country name from number
-            parsed_number = phonenumbers.parse(self.number, None)
-            country_name = geocoder.country_name_for_number(
-                parsed_number, "en")
-        # If no country code is present (signified by lack of "+")
-        else:
-            country_name = "Denmark"
-        return country_name
+        return geocoder.country_name_for_number(self.parsed_number, "en") if self.parsed_number else "Denmark"
